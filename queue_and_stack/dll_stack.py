aaf48b6858bc64601ad4d638cf643de41b40f440
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-14 15:52:40 +0100

    Fix stack siziing

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -14,8 +14,9 @@ class Stack:
         self.size += 1
 
     def pop(self):
-        self.size -= 1
-        return self.storage.remove_from_head()
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
         return self.size
