0121753afa8183d941f335149c080282c0259f2e
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-14 15:52:16 +0100

    Fix queue sizing

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -11,10 +11,13 @@ class Queue:
         self.storage = DoublyLinkedList()
 
     def enqueue(self, value):
+        self.size += 1
         self.storage.add_to_tail(value)
 
     def dequeue(self):
-        return self.storage.remove_from_head()
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
 
     def len(self):
-        return self.storage.length
+        return self.size
