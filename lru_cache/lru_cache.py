a84651270d88dccbf1a553470b979a9d4bc6df18
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-15 14:50:51 +0100

    Add __init__ implementation for LRUCache

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,5 @@
+import DoublyLinkedList from doubly_linked_list
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -7,7 +9,9 @@ class LRUCache:
     to every node stored in the cache.
     """
     def __init__(self, limit=10):
-        pass
+        self.list = DoublyLinkedList()
+        self.limit = limit
+        self.storage = dict()
 
     """
     Retrieves the value associated with the given key. Also
