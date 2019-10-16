9bef1f3146db28dcea2e1eb6e904b70c4aa86fa2
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-16 12:23:23 +0100

    Add swap utility method

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -17,6 +17,9 @@ class Heap:
     def get_size(self):
         pass
 
+    def __swap__(self, i, j):
+        self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
+
     def _bubble_up(self, index):
         pass
 
