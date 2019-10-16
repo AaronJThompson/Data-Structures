47ef97582fb67d35189ba10b1167ae9561a6afb0
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-16 11:59:33 +0100

    Call function recursively on children

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -104,4 +104,7 @@ class BinarySearchTree:
 
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
-        pass
+        if node.left:
+            self.post_order_dft(node.left)
+        if node.right:
+            self.post_order_dft(node.right)
