0cca10e1df38131b7336fdc64b133ba7ff1d0f1e
AaronJThompson <aaronjthompson98@gmail.com>
2019-10-16 12:54:09 +0100

    Fix math without first element

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -2,8 +2,6 @@ import math
 class Heap:
     def __init__(self):
         self.storage = []
-        # This makes the math easier
-        self.storage.append(None)
 
     def insert(self, value):
         self.storage.append(value)
@@ -11,31 +9,31 @@ class Heap:
             self._bubble_up(len(self.storage) - 1)
 
     def delete(self):
-        self.__swap__(1, len(self.storage) - 1) #Move top element to end
+        self.__swap__(0, len(self.storage) - 1) #Move top element to end
         max_node = self.storage.pop()
-        self._sift_down(1)
+        self._sift_down(0)
         return max_node
 
     def get_max(self):
         if self.get_size() > 0:
-            return self.storage[1]
+            return self.storage[0]
 
     def get_size(self):
-        return len(self.storage) - 1
+        return len(self.storage)
 
     def __swap__(self, i, j):
         self.storage[i], self.storage[j] = self.storage[j], self.storage[i]
 
     def _bubble_up(self, index):
-        if index <= 1:
+        if index < 1:
             return
-        parent = math.floor(index / 2)
+        parent = math.ceil(index / 2) - 1
         if self.storage[index] > self.storage[parent]:
             self.__swap__(index, parent)
             self._bubble_up(parent)
 
     def _sift_down(self, index):
-        left_child = index * 2
+        left_child = (index * 2) + 1
         right_child = left_child + 1
 
         if left_child < len(self.storage) - 1 and self.storage[left_child] > self.storage[index]:
